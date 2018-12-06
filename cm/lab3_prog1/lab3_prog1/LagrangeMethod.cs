9ed1e2cb7cc69b053e246e0d80febf38a18580b2
Robert Horvald <roberthorvald@gmail.com>
2018-12-06 16:57:51 +0400

    add task3

---
+++ cm/lab3_prog1/lab3_prog1/LagrangeMethod.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_prog1
+{
+    class LagrangeMethod
+    {
+        readonly PointF[] _points;
+        readonly int _size;
+
+        public LagrangeMethod(PointF[] points, int size)
+        {
+            _points = points;
+            _size = size;
+        }
+
+        private double Calculate(double x)
+        {
+            double lagrangePol = 0;
+
+            for (int i = 0; i < _size; i++)
+            {
+                double basicsPol = 1;
+                for (int j = 0; j < _size; j++)
+                    if (j != i)
+                        basicsPol *= (x - _points[j].X) / (_points[i].X - _points[j].X);
+                    lagrangePol += basicsPol * _points[i].Y;
+            }
+
+            return lagrangePol;
+        }
+
+        public double Resolve(double x)
+            => this.Calculate(x);
+    }
+}
