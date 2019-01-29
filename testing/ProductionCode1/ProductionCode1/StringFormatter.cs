3350ab7864a573abd0ff117615f109e53b53c840
Robert Horvald <roberthorvald@gmail.com>
2019-01-29 20:30:34 +0400

    lab1

---
+++ testing/ProductionCode1/ProductionCode1/StringFormatter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProductionCode1
+{
+    public class StringFormatter
+    {
+        readonly string[] _keyWords = new[] { "select", "insert", "update", "delete"}; 
+
+        public string SafeString(string s)
+        {
+            if (s == null)
+                throw new NullReferenceException();
+
+            if (s == string.Empty)
+                return s;
+
+            string localString = s.Replace("'", "''");
+
+            foreach (var keyWord in _keyWords)
+                localString = new Regex(keyWord, RegexOptions.IgnoreCase).Replace(localString, keyWord.ToUpper());
+
+            return localString;
+        }
+    }
+}
