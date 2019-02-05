405738d685dce49025cf152c404a82c46a8bec81
Robert Horvald <roberthorvald@gmail.com>
2019-02-06 01:16:42 +0400

    lab2

---
+++ testing/ProductionCode2/ProductionCode2.Tests/Fakes/FakeStubFileService.cs
@@ -0,0 +1,23 @@
+﻿using ProductionCode;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionCode2.Tests.Fakes
+{
+    public class FakeStubFileService
+        : IFileService
+    {
+        public int ReturnValueMergeTemporaryFiles { get; set; }
+        public int ReturnValueRemoveTemporaryFiles { get; set; }
+
+        public int MergeTemporaryFiles(string dir)
+            => this.ReturnValueMergeTemporaryFiles;
+        
+        public int RemoveTemporaryFiles(string dir)
+            => this.ReturnValueRemoveTemporaryFiles;
+    }
+}
---
+++ testing/ProductionCode2/ProductionCode2/FileService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionCode
+{
+    public class FileService
+        : IFileService
+    {
+        readonly string _filterExtension = ".tmp";
+        readonly string _outputFilename = "/backup.tmp";
+        readonly string _errorLogFilename = "/error.log";
+        readonly string _removeFilesListFilename = "/error.log";
+
+        public int MergeTemporaryFiles(string dir)
+        {
+            IEnumerable<string> files = Directory.GetFiles(dir).Where(t => t.EndsWith(_filterExtension));
+
+            if (files.Count() == 0)
+                return 0;
+
+            File.WriteAllText(Path.Combine(dir, _outputFilename), files.Aggregate((t,k) => string.Concat(t,k)));
+            files.ToList().ForEach(t => File.Delete(t));
+
+            return files.Count();
+        }
+
+        public int RemoveTemporaryFiles(string dir)
+        {
+            IEnumerable<string> files = File.ReadAllLines(Path.Combine(dir, _removeFilesListFilename));
+
+            long sizeDeletedFiles = default(int);
+            files.ToList()
+                .ForEach(t => 
+                {
+                    try
+                    {
+                        sizeDeletedFiles =+ new FileInfo(t).Length;
+                        File.Delete(t);
+                    }
+                    catch
+                    {
+                        File.AppendAllText(Path.Combine(dir, _errorLogFilename), $"Фaйл {Path.GetFileNameWithoutExtension(t)} не был найден.{Environment.NewLine}");
+                    }
+                });
+
+            return (int)sizeDeletedFiles;
+        }
+    }
+}
